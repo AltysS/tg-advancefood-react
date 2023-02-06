c776f8032eb6889907bbcfc7558ef33230fb907e
Oleksandr Altynnikov <siolent.ou@gmail.com>
2023-02-06 17:13:41 +0200

    fix: fix function routes

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -18,8 +18,8 @@ const Form = () => {
   }, []);
 
   useEffect(() => {
-    tg.WebApp.onEvent("mainButtonClicked", onSendData);
-    return tg.WebApp.offEvent("mainButtonClicked", onSendData);
+    tg.onEvent("mainButtonClicked", onSendData);
+    return () => tg.offEvent("mainButtonClicked", onSendData);
   }, []);
 
   useEffect(() => {
