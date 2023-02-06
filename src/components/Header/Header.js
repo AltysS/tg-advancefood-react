9287eb840df12df78c2b46a6274529f35b60786c
Oleksandr Altynnikov <siolent.ou@gmail.com>
2023-02-06 13:01:07 +0200

    feat: add button components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "../Button/Button";
+
+const tg = window.Telegram.WebApp;
+
+const Header = () => {
+  const onClose = () => {
+    tg.close();
+  };
+  return (
+    <div className={"header"}>
+      <Button onClick={onclose}>Закрити</Button>
+      <span className={"username"}>tg.initDataUnsafe?.user?.username</span>
+    </div>
+  );
+};
+
+export default Header;
