64630d1b89a4d1e38da3b2db312be0702b76a39c
Oleksandr Altynnikov <siolent.ou@gmail.com>
2023-02-08 16:25:35 +0200

    feat: add product q-ty func and styling to it

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ProductItem.css";
 import Button from "../../components/Button/Button";
 
-const ProductItem = ({ product, onAdd }) => {
+const ProductItem = ({ orderedQty, product, onAdd, onDelete, needUpdate }) => {
+  console.log(orderedQty);
+  const onDeleteHandler = () => {
+    onDelete(product);
+  };
   const onAddHandler = () => {
     onAdd(product);
   };
@@ -12,15 +16,23 @@ const ProductItem = ({ product, onAdd }) => {
         src="https://detta.com.ua/upload/iblock/41a/8410650170480.png"
         className={"img"}
       />
+      {orderedQty > 0 && (
+        <div className={"qtyRoundFigure"}>{product.orderedQty}</div>
+      )}
       <div className={"title"}>{product.title}</div>
       <div className={"description"}>{product.description}</div>
       <div className={"price"}>
         <span>
           Ціна: <b>{product.price}</b>
         </span>
-        <Button className={"add-btn"} onClick={onAddHandler}>
-          Додати до кошика
-        </Button>
+        <div className={"btn-container"}>
+          <Button className={"btn"} onClick={onDeleteHandler}>
+            -
+          </Button>
+          <Button className={"btn"} onClick={onAddHandler}>
+            +
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductItem from "../ProductItem/ProductItem";
 import "./ProductList.css";
 import useTelegram from "../../hooks/useTelegram";
@@ -10,6 +10,7 @@ const products = [
     price: 495,
     description:
       "Advance Dog Yorkshire - високоякісний збалансований повнораціонний корм спеціально розроблений для забезпечення харчових потреб собак породи Йоркширський терsєр в віці від 1 року",
+    orderedQty: 0,
   },
   {
     id: 2,
@@ -17,6 +18,7 @@ const products = [
     price: 845,
     description:
       "Advance Dog Yorkshire - високоякісний збалансований повнораціонний корм спеціально розроблений для забезпечення харчових потреб собак породи Йоркширський терsєр в віці від 1 року",
+    orderedQty: 0,
   },
   {
     id: 3,
@@ -24,6 +26,7 @@ const products = [
     price: 795,
     description:
       "Advance Dog Yorkshire - високоякісний збалансований повнораціонний корм спеціально розроблений для забезпечення харчових потреб собак породи Йоркширський терsєр в віці від 1 року",
+    orderedQty: 0,
   },
   {
     id: 4,
@@ -31,6 +34,7 @@ const products = [
     price: 695,
     description:
       "Advance Dog Yorkshire - високоякісний збалансований повнораціонний корм спеціально розроблений для забезпечення харчових потреб собак породи Йоркширський терsєр в віці від 1 року",
+    orderedQty: 0,
   },
   {
     id: 5,
@@ -38,48 +42,90 @@ const products = [
     price: 595,
     description:
       "Advance Dog Yorkshire - високоякісний збалансований повнораціонний корм спеціально розроблений для забезпечення харчових потреб собак породи Йоркширський терsєр в віці від 1 року",
+    orderedQty: 0,
   },
 ];
 
 const getTotalPrice = (items) => {
   return items.reduce((acc, item) => {
-    return (acc += item.price);
+    return (acc += item.price * item.orderedQty);
   }, 0);
 };
 
 const ProductList = () => {
-  const [addedItems, setAddedItems] = useState([]);
-
+  const [addedItems, setAddedItems] = useState();
+  const [needUpdate, setNeedUpdate] = useState(false);
   const { tg } = useTelegram();
 
+  useEffect(() => {
+    setNeedUpdate(!needUpdate);
+  }, [addedItems]);
+  useEffect(() => {
+    setAddedItems(products);
+  }, []);
+
+  const onDelete = (product) => {
+    let totalQty = 0;
+    const newItemsArr = addedItems.map((el) => {
+      if (el.id === product.id && product.orderedQty !== 0) {
+        product.orderedQty -= 1;
+      } else {
+        totalQty += product.orderedQty;
+      }
+      return el;
+    });
+    totalQty === 0 && tg.MainButton.hide();
+    setAddedItems(newItemsArr);
+    setNeedUpdate(!needUpdate);
+  };
+
   const onAdd = (product) => {
-    const alreadyAdded = addedItems.find((item) => item.id === product.id);
-    let newItems = [];
-    if (alreadyAdded) {
-      newItems = addedItems.filter((item) => item.id !== product.id);
-    } else {
-      newItems = [...addedItems, product];
-    }
-
-    setAddedItems(newItems);
-
-    console.log(getTotalPrice(newItems));
-
-    if (newItems.length === 0) {
-      tg.MainButton.hide();
-    } else {
-      tg.MainButton.setParams({
-        text: `Замовити товар на сумму ${getTotalPrice(newItems)}`,
-      });
-      tg.MainButton.show();
-    }
+    const newItemsArr = addedItems;
+    newItemsArr.map((el) => {
+      if (el.id === product.id) {
+        return (el.orderedQty += 1);
+      }
+      return el;
+    });
+    setAddedItems(newItemsArr);
+    setNeedUpdate(!needUpdate);
+    tg.MainButton.setParams({
+      text: `Придбати на сумму ${getTotalPrice(newItemsArr)}`,
+    });
+    tg.MainButton.show();
+    // const alreadyAdded = addedItems.find((item) => item.id === product.id);
+    // let newItems = [];
+    // if (alreadyAdded) {
+    //   newItems = addedItems.filter((item) => item.id !== product.id);
+    // } else {
+    //   newItems = [...addedItems, product];
+    // }
+    // setAddedItems(newItems);
+    // console.log(getTotalPrice(newItems));
+    // if (newItems.length === 0) {
+    //   tg.MainButton.hide();
+    // } else {
+    //   tg.MainButton.setParams({
+    //     text: `Замовити товар на сумму ${getTotalPrice(newItems)}`,
+    //   });
+    //   tg.MainButton.show();
+    // }
   };
   return (
-    <div className={"list"}>
-      {products.map((item) => (
-        <ProductItem product={item} onAdd={onAdd} />
-      ))}
-    </div>
+    addedItems && (
+      <div className={"list"}>
+        {addedItems.map((item) => (
+          <ProductItem
+            orderedQty={item.orderedQty}
+            key={item.id}
+            product={item}
+            onAdd={onAdd}
+            onDelete={onDelete}
+            needUpdate={needUpdate}
+          />
+        ))}
+      </div>
+    )
   );
 };
 
