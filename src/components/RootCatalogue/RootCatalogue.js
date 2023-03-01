eddba287ef4c49d4bdcfebf6fd4f54b2fd0a0142
Oleksandr Altynnikov <siolent.ou@gmail.com>
2023-03-01 12:25:39 +0200

    fix: add classname to catalogue categiries

--- src/components/RootCatalogue/RootCatalogue.js
+++ src/components/RootCatalogue/RootCatalogue.js
@@ -87,6 +87,7 @@ const RootCatalogue = () => {
               >
                 <div>
                   <img
+                    className="categoryImage"
                     style={{ height: "150px" }}
                     src={
                       "https://detta.com.ua/sections_img/" + formImageLink(id)
